8e3d8fc8ae549b4d2557cd925b9234c9ae1cef8c
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-04 13:20:05 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name = "Josh") {
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name = Josh, language = JavaScript) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name = Josh, language = JavaScript) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
